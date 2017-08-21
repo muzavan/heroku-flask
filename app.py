b6694e6f2ef5583d3efcfc8ab45f001838d9d02e
Reza <reza.irvanda@xtremax.com>
2017-08-21 23:04:11 +0700

    simplify generic

--- app.py
+++ app.py
@@ -160,49 +160,23 @@ def send_generic(recipient_id):
                 "elements":[
                    {
                     "title":"Welcome to Peter\'s Hats",
-                    "image_url":"https://petersfancybrownhats.com/company_image.png",
-                    "subtitle":"We\'ve got the right hat for everyone.",
                     "default_action": {
                       "type": "web_url",
                       "url": "https://peterssendreceiveapp.ngrok.io/view?item=103",
                       "messenger_extensions": True,
                       "webview_height_ratio": "tall",
                       "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-                    },
-                    "buttons":[
-                      {
-                        "type":"web_url",
-                        "url":"https://petersfancybrownhats.com",
-                        "title":"View Website"
-                      },{
-                        "type":"postback",
-                        "title":"Start Chatting",
-                        "payload":"DEVELOPER_DEFINED_PAYLOAD"
-                      }
-                    ]
+                    }
                   },
                     {
                         "title": "Welcome to Humbala",
-                        "image_url": "https://petersfancybrownhats.com/company_image.png",
-                        "subtitle": "We\'ve got the right hat for everyone.",
                         "default_action": {
                             "type": "web_url",
                             "url": "https://peterssendreceiveapp.ngrok.io/view?item=103",
                             "messenger_extensions": True,
                             "webview_height_ratio": "tall",
                             "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-                        },
-                        "buttons": [
-                            {
-                                "type": "web_url",
-                                "url": "https://petersfancybrownhats.com",
-                                "title": "View Website"
-                            }, {
-                                "type": "postback",
-                                "title": "Start Chatting",
-                                "payload": "DEVELOPER_DEFINED_PAYLOAD"
-                            }
-                        ]
+                        }
                     }
                 ]
               }
