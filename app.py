e37bae917967e73e67a74fce7f5b72c23914bc0c
Reza <reza.irvanda@xtremax.com>
2017-08-23 10:21:39 +0700

    Add sender action

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import json
+import time
 
 import requests
 from flask import Flask, request
@@ -187,6 +188,28 @@ def send_generic(recipient_id):
         log(r.status_code)
         log(r.text)
 
+def send_sender_action(recipient_id):
+    log("sending message to {recipient}".format(recipient=recipient_id))
+
+    params = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "sender_action": "typing_on"
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        log(r.status_code)
+        log(r.text)
+
+    time.sleep(15)
+    send_message(recipient_id,"This is after 15 seconds")
 
 
 def log(message):  # simple wrapper for logging to stdout on heroku
